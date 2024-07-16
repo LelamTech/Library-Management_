c0d7d7f9714525b6ba049b02ccf697639953cb55
LelamKinsTech <123534379+LelamTech@users.noreply.github.com>
2024-07-17 00:36:36 +0300

    Add files via upload

---
+++ Library Management System 2.0/Library Management System 2.0/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Book Title"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 133)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 32)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Author"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(172, 63)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(163, 33)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(172, 133)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(163, 33)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 191)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 32)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Year Published"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 242)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 32)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Genre"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(172, 191)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(163, 33)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(172, 239)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(163, 33)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 318)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 37)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(126, 318)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 37)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+        Me.Button3.Location = New System.Drawing.Point(317, 318)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 37)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Delete"
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(341, 63)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(447, 245)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(224, 318)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 37)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Display"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Uighur", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(216, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(375, 48)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Kabu Library Management System "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(862, 721)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Uighur", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Library Management System 2.0/Library Management System 2.0/Form1.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim i As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "Data Source=TSUNAMI;Initial Catalog=LibraryDB;Integrated Security=True;Encrypt=False"
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+        Disp_Data()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "Insert into Books values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "')"
+        cmd.ExecuteNonQuery()
+        MsgBox("Record Inserted Succesfully")
+        Disp_Data()
+    End Sub
+    Public Sub Disp_Data()
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from Books"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+
+
+
+    End Sub
+
+    Private Sub dgvBooks_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub dgvBooks_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Try
+            If con.State = ConnectionState.Open Then
+                con.Close()
+
+            End If
+            con.Open()
+            i = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+            cmd = con.CreateCommand
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select * from Books where id=" & i & ""
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+            Dim dr As SqlClient.SqlDataReader
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            While dr.Read
+                TextBox1.Text = dr.GetString(1).ToString()
+                TextBox2.Text = dr.GetString(2).ToString()
+                TextBox3.Text = dr.GetInt32(3).ToString()
+                TextBox4.Text = dr.GetString(4).ToString()
+            End While
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "update Books set Title='" + TextBox1.Text + "',Author='" + TextBox2.Text + "',YearPublished='" + TextBox3.Text + "',Genre='" + TextBox4.Text + "'where id=" & i & ""
+        cmd.ExecuteNonQuery()
+
+        Disp_Data()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Only Type In Title when using delete")
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from Books where Title='" + TextBox1.Text + "'"
+        cmd.ExecuteNonQuery()
+        Disp_Data()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from Books"
+        cmd.ExecuteNonQuery()
+        Disp_Data()
+    End Sub
+End Class
